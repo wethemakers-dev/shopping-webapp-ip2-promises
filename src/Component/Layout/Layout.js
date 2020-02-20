17bdc9853582f6d8559ab5d3c77a4bff2a62df73
Yazanwidyan <yazanwidyan1@gmail.com>
2020-02-20 23:34:58 +0200

    make file for server and client

--- src/Component/Layout/Layout.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Content } from "rsuite";
-
-export const Layout = props => (
-  <Content className="body-container">{props.children}</Content>
-);
--- src/Container/Familyshare/Familyshare.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Familyshare extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
-
-export default Familyshare;
